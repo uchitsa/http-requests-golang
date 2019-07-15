888d7fd0801f0ec34b460b47cdd1c6cefb525d01
uchitsa <uchitsa@ya.ru>
2019-07-15 18:30:55 +0300

    Try http requests

---
+++ httpPostForm.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+func main() {
+	formData := url.Values{
+		"name": {"Ivan"},
+	}
+
+	resp, err := http.PostForm("https://httpbin.org/post", formData)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var result map[string]interface{}
+
+	json.NewDecoder(resp.Body).Decode(&result)
+
+	log.Println(result["form"])
+}
